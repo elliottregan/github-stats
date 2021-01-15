1ca85aa3232fb40665143f0ebe8e5c67d708628a
Elliott Regan <elliott@elliottregan.com>
2021-01-14 22:57:21 -0700

    sort langs

--- index.ts
+++ index.ts
@@ -2,29 +2,31 @@ import { loadConfig } from './config/index';
 loadConfig();
 
 import { getRepoLangSizes, getUserRepos } from "./src/httpSvc/index";
-import { mergeLanguages, formatLanguageOutput } from './src/dataSvc/index';
+import { mergeLanguages, formatLanguageOutput, sortLanguages } from './src/dataSvc/index';
 import { writeFile } from 'fs';
 
 const user = process.argv.slice(2)[0] || 'elliottregan';
 
-const getUserLanguageStats = async (username:string = 'elliottregan'):Promise<any> => {
+const getUserLanguageStats = async (username:string):Promise<any> => {
   const repos = await getUserRepos(username);
   const sizes = await Promise.all(repos.map(r => getRepoLangSizes(username, r.name)));
   return sizes
 }
 
-const createOutputString = (languageData: any[]):string => {
+const createOutputString = (languageData:any):string => {
   return `Language repartition for ${user}
-${formatLanguageOutput(mergeLanguages(languageData))}`
+${formatLanguageOutput(languageData)}`
 }
 
-const createOutputFile = (languageStats: string) => {
+const createOutputFile = (languageStats:string) => {
   writeFile(`./dist/${user}.txt`, languageStats, (err) => {
     if (err) console.error(err)
   });
 }
 
 getUserLanguageStats(user)
+  .then(mergeLanguages)
+  .then(sortLanguages)
   .then(createOutputString)
   .then(createOutputFile);
 
--- src/dataSvc/index.ts
+++ src/dataSvc/index.ts
@@ -1,17 +1,29 @@
+type LangBytePair = Array<[string, number]>;
+
 export const pickKeys = ({ name = '', url = '' }): { name:string, url:string } => ({ name, url });
 
-export const mergeLanguages = (data:Array<any>) => {
-  return data.reduce((allRepoLangs, repoLangs) => {
+export const mergeLanguages = (data:Array<any>): Array<any> => {
+  const langSums = data.reduce((allRepoLangs, repoLangs) => {
     for (let [key, value] of Object.entries(repoLangs)) {
       allRepoLangs[key] = allRepoLangs[key] ? allRepoLangs[key] + value : value;
     }
     return allRepoLangs;
   }, {})
+  return Object.entries(langSums);
 }
 
-export const formatLanguageOutput = (languageData:{bytes:number, lang:string}) => {
+export const sortLanguages = (data:Array<LangBytePair>):Array<LangBytePair> => {
+  const sortedKeys = data.sort((a:LangBytePair, b:LangBytePair) => {
+    if (a[1] < b[1]) return 1;
+    if (a[1] > b[1]) return -1;
+    return 0;
+  });
+  return data;
+}
+
+export const formatLanguageOutput = (languageData:Array<LangBytePair>) => {
   let output = '';
-  Object.entries(languageData).map(([bytes, lang]) => {
+  languageData.map(([bytes, lang]) => {
     output += `* ${bytes}: ${lang}
 `
   });
